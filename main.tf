c5c1c5d18fbe27ee1936916e72dc51a3eb412284
udit-uniyal <udituniyal71@gmail.com>
2024-03-04 19:50:45 +0530

    Added Terraform scripts

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
