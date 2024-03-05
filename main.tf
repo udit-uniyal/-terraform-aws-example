7e7b512ea16b2ba34e3ac3b653e82b384e19993e
udit-uniyal <udituniyal71@gmail.com>
2024-03-05 10:19:09 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,5 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-}
-
-resource "aws_security_group" "allow_specific" {
-  name        = "allow_specific"
-  description = "Allow specific inbound traffic"
-  
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["your-ip-address/32"]
-  }
+resource "aws_s3_bucket" "example" {
+  bucket = "my-insecure-bucket"
+  acl    = "private"
 }
 
