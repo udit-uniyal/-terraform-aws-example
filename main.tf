5110eb8180fda6f0f4ee6fc576a7a10d2a98e653
udit-uniyal <udituniyal71@gmail.com>
2024-03-05 10:19:42 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "my-insecure-bucket"
+  bucket = "my-secure-bucket"
   acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
